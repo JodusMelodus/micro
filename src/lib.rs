0569b0e5dcca8810f5b7bcec97f22b75b8cf5796
JodusMelodus <philipgricius@gmail.com>
2026-01-11 15:05:34 +0200

    impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data, DeriveInput, Fields, Path, parse_macro_input};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro_derive(FromDTO, attributes(from))]
+pub fn from_dto_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let mut from_path: Option<Path> = None;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for attribute in &input.attrs {
+        if attribute.path().is_ident("from") {
+            if let Ok(path) = attribute.parse_args::<Path>() {
+                from_path = Some(path)
+            }
+        }
     }
+
+    let path = from_path.expect("The #[from(path)] attribute is required for FromDTO");
+
+    let fields = if let Data::Struct(s) = input.data {
+        if let Fields::Named(f) = s.fields {
+            f.named
+        } else {
+            panic!("FromDTO only supports structs with named fields");
+        }
+    } else {
+        panic!("FromDTO can only be used on structs");
+    };
+
+    let field_idents = fields.iter().map(|f| &f.ident);
+
+    let output = quote! {
+        impl From<#path> for #name {
+            fn from(value: #path) -> Self {
+                Self {
+                    #(
+                        #field_idents: value.#field_idents.into(),
+                    )*
+                }
+            }
+        }
+    };
+
+    TokenStream::from(output)
 }
---
+++ tests/test.rs
@@ -0,0 +1,24 @@
+use micro::FromDTO;
+
+#[test]
+fn test1() {
+    struct Test1 {
+        name: String,
+        age: u8,
+    }
+
+    #[derive(FromDTO)]
+    #[from(Test1)]
+    struct Test2 {
+        name: String,
+        age: u16,
+    }
+
+    let t1 = Test1 {
+        name: "John".to_string(),
+        age: 5,
+    };
+
+    let t2: Test2 = t1.into();
+}
+
